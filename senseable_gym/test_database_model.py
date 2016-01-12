90ed46701fb2c92e86879ed498531982f81f294a
TJ DeVries <devries.timothyj@gmail.com>
2016-01-12 13:04:34 -0500

    Restructure the python folders
    This is so that the code can be contained in a better location, where we
    can store the tests more correctly.
    
    I'm still not sure if this is the exact way to set it up, but I think it
    is a step in the right direction. It lets us now import things from
    senseable_gym, instead of each module individually.

---
+++ senseable_gym/test_database_model.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Built-in Imports
+import unittest
+
+# Local Imports
+from senseable_gym.sg_database.database import DatabaseModel
+
+
+class TestDatabaseModel(unittest.TestCase):
+    def setUp(self):
+        """TODO: Docstring for setUp.
+
+
+        """
+        db = DatabaseModel('testdb', 'team14')
+
+        db.add_machine('machine1')
