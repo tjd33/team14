2d9d00a404fe9ed6ece2b9754e582e4b12d870be
TJ DeVries <devries.timothyj@gmail.com>
2016-01-27 14:52:30 -0500

    Begin working on relationships and users

--- senseable_gym/sg_util/user.py
+++ senseable_gym/sg_util/user.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # Standard Imports
-from sqlalchemy import Column, Integer, Sequence
+from sqlalchemy import Column, Integer, Sequence, String
 
 # Senseable Gym Imports
 from senseable_gym.sg_util.base import Base
@@ -12,3 +12,8 @@ class User(Base):
     __tablename__ = 'user'
 
     id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
+    user_name = Column(String(32))
+    first_name = Column(String(32))
+    last_name = Column(String(32))
+
+
