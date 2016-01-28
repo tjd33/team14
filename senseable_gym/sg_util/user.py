c872809e1f4bd2cee3f14a56e7cc492604c96100
TJ DeVries <devries.timothyj@gmail.com>
2016-01-27 20:38:24 -0500

    Further work in the user and relationship area

--- senseable_gym/sg_util/user.py
+++ senseable_gym/sg_util/user.py
@@ -11,22 +11,22 @@ from senseable_gym.sg_util.base import Base
 class User(Base):
     __tablename__ = 'user'
 
+    def __init__(self, user_name: str,  first_name: str, last_name: str):
+        self._user_name = user_name
+        self._first_name = first_name
+        self._last_name = last_name
+
     id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
     user_name = Column(String(32))
     first_name = Column(String(32))
     last_name = Column(String(32))
 
-    def __init__(self, user_name, first_name, last_name):
-        self._user_name = user_name
-        self._first_name = first_name
-        self._last_name = last_name
-
     @property
-    def full_name(self):
+    def full_name(self) -> str:
         return self.first_name + ' ' + self.last_name
 
     @property
-    def user_name(self):
+    def user_name(self) -> str:
         return self._user_name
 
     @user_name.setter
@@ -34,7 +34,7 @@ class User(Base):
         self._user_name = value
 
     @property
-    def first_name(self):
+    def first_name(self) -> str:
         return self._first_name
 
     @first_name.setter
@@ -42,7 +42,7 @@ class User(Base):
         self._first_name = value
 
     @property
-    def last_name(self):
+    def last_name(self) -> str:
         return self._last_name
 
     @last_name.setter
