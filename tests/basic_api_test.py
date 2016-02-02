3aff59070e60fd2f18db6f38f5e6147c490841bd
TJ DeVries <devries.timothyj@gmail.com>
2016-02-02 09:36:35 -0500

    Show a basic use case of the DatabaseModel

---
+++ tests/basic_api_test.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+This will provide a basic test of the the api provided by the senseable gym.
+    This script will provide a dictionary that has structure, history
+    and relationships.
+
+It will also provide a reference for how to use the API that the senseable
+    gym provides.
+"""
+
+# Standard Imports
+
+# Third Party Imports
+
+# Senseable Gym Imports
+from senseable_gym.sg_database.database import DatabaseModel
+from senseable_gym.sg_util.machine import Machine, MachineStatus, MachineType
+
+# Code begins here
+
+# Create your own database model
+#   You specify the name of the database and then the user accessing the database
+#   TODO: Update with more information as the __init__ becomes more complete
+db = DatabaseModel('dbname', 'example')
+
+for machine_id in range(10):
+    if machine_id % 2 == 0:
+        m_type = MachineType.BICYCLE
+        m_status = MachineStatus.BUSY
+    else:
+        m_type = MachineType.TREADMILL
+        m_status = MachineStatus.UNKNOWN
+
+    temp_machine = Machine(type=m_type, location=[machine_id, machine_id, 1])
+    temp_machine.status = m_status
+
+    db.add_machine(temp_machine)
+
+machine_list = db.get_machines()
+
+for machine in machine_list:
+    print(machine)
