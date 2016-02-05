ec0eaaacd0c3a43159c443cbcc8668aa2f61312b
TJ DeVries <devries.timothyj@gmail.com>
2016-02-05 14:47:24 -0500

    Clean up several incorrect commits

--- senseable_gym/test/test_reservation.py
+++ senseable_gym/test/test_reservation.py
@@ -10,12 +10,12 @@ from datetime import datetime
 # Local Imports
 from senseable_gym.sg_database.database import DatabaseModel
 from senseable_gym.sg_util.machine import Machine, MachineType, MachineStatus
-from senseable_gym.sg_util.User import User
-from senseable_gym.sg_util.Reservation import Reservation
+from senseable_gym.sg_util.user import User
+from senseable_gym.sg_util.reservation import Reservation
 
-class TestReservation(unittest.TestClass):
+class TestReservation(unittest.TestCase):
     def setUp(self):
-        self.db = DatabaseModel('testdb', 'team14')
+        self.db = DatabaseModel(None, 'team14')
 
         self.m1 = Machine(MachineType.TREADMILL, location=[2, 2, 3])
         self.u1 = User('1', 'f1', 'l1')
