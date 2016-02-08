0b5d6b1143f3fbc29897c3b8410c10a8398dc502
Daniel Dehoog <danielgdehoog@gmail.com>
2016-02-07 22:27:23 -0500

    added server functionality to the client and client funcitonality to the server

--- senseable_gym/sg_network/server.py
+++ senseable_gym/sg_network/server.py
@@ -2,6 +2,7 @@ import sys
 import socketserver
 import socket
 import struct
+import os
 from threading import Thread
 from Reservation import Reservation
 import pickle
@@ -10,6 +11,33 @@ import pickle
 from senseable_gym.sg_database.database import DatabaseModel
 from senseable_gym.sg_util.machine import Machine, MachineType, MachineStatus
 from senseable_gym.sg_util.user import User
+
+class ServerClient:
+	def __init__(self, host, hostPort):
+		self.server_address = (host, hostPort)
+		print ('connecting to %s port %s' % self.server_address)
+		
+	def pickleAndSend(self, object):
+		# Create a TCP/IP socket to the server
+		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		sock.connect(self.server_address)
+		data_string = pickle.dumps(object, -1)
+		try:
+			size = len(data_string)
+			print(size)
+			size = struct.pack("I", socket.htonl(size))
+			sock.sendall(size)
+			data = sock.recv(8)
+			if data != b'received':
+				print("communication error")
+			sock.sendall(data_string)
+			data = sock.recv(8)
+			if data != b'received':
+				print("communication error")
+
+		finally:
+			sock.close()
+
 
 class service(socketserver.BaseRequestHandler):
 	def handle(self):
@@ -44,11 +72,26 @@ if len(sys.argv) < 2:
 	host = 'localhost'
 else:
 	host = sys.argv[1]
+client = ServerClient(host, 20000)
 t = ThreadedTCPServer((host,10000), service)
 t.allow_reuse_address = True
 print('created server')
 
+def runTCPServer():
+	try:
+		t.serve_forever()
+	finally:
+		pass
+
+thread = Thread(target = runTCPServer).start()
+print("server running")
+
+res = Reservation("pgriff", 1200, 150)
 try:
-	t.serve_forever()
-except KeyboardInterrupt:
-	t.server_close()
+	client.pickleAndSend(res)
+except ConnectionRefusedError:
+	print ('connection refused')
+	# should this remember and retry, or discard and do complete refresh when connection is achieved
+os.system('pause')
+t.shutdown()
+t.server_close()
