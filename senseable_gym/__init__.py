1e43ffce6c1d0df7fc278711167ff2fe2fe752bf
TJ DeVries <devries.timothyj@gmail.com>
2016-02-10 13:27:53 -0500

    Add documentation to logger setup

--- senseable_gym/__init__.py
+++ senseable_gym/__init__.py
@@ -1,9 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import logging
 
-logger = logging.getLogger('senseable_logger')
+# Senseable Gym Logging Configuration
+
+# This is the name that all other loggers must have as their root
+global_logger_name = 'senseable_logger'
+
+# Create the base logger
+logger = logging.getLogger(global_logger_name)
+
+# {{{ Handlers
 handler = logging.StreamHandler()
+
+# }}}
+# {{{ Formatters
 formatter = logging.Formatter(
         '| %(name)-12s | %(levelname)-8s | %(message)s')
+
+# }}}
+# {{{ Attach Handlers and Formatters
 handler.setFormatter(formatter)
 logger.addHandler(handler)
+# }}}
+# {{{ Level Management
 logger.setLevel(logging.ERROR)
+# }}}
