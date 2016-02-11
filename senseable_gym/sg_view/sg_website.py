f1af3865196e48fbfcde7f47cb9a600e139305b6
TJ DeVries <devries.timothyj@gmail.com>
2016-02-10 21:28:04 -0500

    Fix small rebase problem

--- senseable_gym/sg_view/sg_website.py
+++ senseable_gym/sg_view/sg_website.py
@@ -3,13 +3,19 @@ from flask import Flask, url_for, render_template
 
 #Local Imports
 from senseable_gym.sg_database.database import DatabaseModel
+from senseable_gym.sg_util.machine import Machine, MachineType, MachineStatus
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    machines = DatabaseModel.get_machines()
-    return render_template('machine_view.html', machines=machines)
+    db=DatabaseModel('name','ryan')
+    machine_1=Machine(MachineType.TREADMILL, [1, 1, 1])
+    db.add_machine(machine_1)
+    machine_2=Machine(MachineType.TREADMILL, [1, 1, 2])
+    db.add_machine(machine_2)
+    machine_list=db.get_machines()
+    return render_template('machine_view.html', machines=machine_list)
 
 @app.route('/index/')
 def index():
@@ -20,9 +26,9 @@ def index():
 def hello(name=None):
     return render_template('hello.html', name=name)
 
-@app.route('/admin/')
-def admin():
-    return 'Admin Page'
+@app.route('/example/')
+def example():
+	return render_template('example.html')
 
 @app.route('/login/')
 def login():
@@ -33,6 +39,7 @@ def page_not_found(error):
 	return render_template('page_not_found.html'), 404
 
 if __name__ == '__main__':
+	#go to "localhost:5000/" to view pages
     app.debug = True
     app.run()
     
