5337b3a32ed5f7b44c8b6e3b9baf275856381d2c
Paul Griffioen <pmgriff6@gmail.com>
2016-03-05 11:52:53 -0500

    enabled serial port communication and signal processing

--- senseable_gym/sg_util/signal_processing.py
+++ senseable_gym/sg_util/signal_processing.py
@@ -1,14 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import serial    # http://pyserial.readthedocs.org/en/latest/pyserial.htmlp
+from tkinter import *
 
 # Specify how many different kinds of data are in a data block
 # A data block is a group of data, with each data point being on a new line
 # Data blocks are separated by an empty line
 rowlength = 6
-filename = '/Users/paul/Desktop/ZTerm/Weight_Machine' # Name of file to open
+filename = '/Users/paul/Desktop/ZTerm/Treadmill_Front' # Name of file to open
 fields = ['Gyro X (deg/sec)', 'Gyro Y (deg/sec)', 'Gyro Z (deg/sec)', 'Accel X (G)', 'Accel Y (G)', 'Accel Z (G)']
 
-def read_text_file_data():
+def read_text_file_data(filename):
 	# Read file into list
 	file = open(filename, 'r')
 	datastring = file.read()
@@ -37,22 +39,28 @@ def read_text_file_data():
 	col = 0
 	index = 0
 	line = begin + 2
+	no_newline = []
+	unwanted_newline = []
+	invalid_number = []
 	for each_item in datalist:
 		if row == rowlength:
 			if each_item != '':
-				print('ERROR - LINE ' + str(line) + ': NO NEWLINE WHERE THERE SHOULD BE ONE')
+				# print('ERROR - LINE ' + str(line) + ': NO NEWLINE WHERE THERE SHOULD BE ONE')
+				no_newline.append(line)
 				return
 			row = 0
 			col += 1
 		else:
 			if each_item == '':
-				print('ERROR - LINE ' + str(line) + ': NEWLINE WHERE THERE SHOULD NOT BE ONE')
+				# print('ERROR - LINE ' + str(line) + ': NEWLINE WHERE THERE SHOULD NOT BE ONE')
+				unwanted_newline.append(line)
 				row = 0
 			elif is_number(each_item) == 0:
-				print('ERROR - LINE ' + str(line) + ': LINE MUST CONTAIN A VALID NUMBER')
+				# print('ERROR - LINE ' + str(line) + ': LINE MUST CONTAIN A VALID NUMBER')
+				invalid_number.append(line)
 				row = 0
 			else:
-				matrix_data[row][col] = each_item
+				matrix_data[row][col] = abs(float(each_item))
 				row += 1
 		line += 1
 
@@ -61,14 +69,20 @@ def read_text_file_data():
 		# print(matrix_data[c])
 
 	# Return matrix
-	return matrix_data
+	return matrix_data, no_newline, unwanted_newline, invalid_number
 
 def plot_sensor_data(matrix_data):
-	for k in range(0, rowlength):
-		plt.plot(range(1,len(matrix_data[0])+1), matrix_data[k])
-		plt.xlabel('Time')
-		plt.ylabel(fields[k])
-		plt.show()
+	gyro_data = matrix_data[0] + matrix_data[1] + matrix_data[2]
+	accel_data = matrix_data[3] + matrix_data[4] + matrix_data[5]
+	plt.plot(range(1,len(gyro_data)+1), gyro_data)
+	plt.show()
+	plt.plot(range(1,len(accel_data)+1), accel_data)
+	plt.show()
+	# for k in range(0, rowlength):
+	# 	plt.plot(range(1,len(matrix_data[0])+1), matrix_data[k])
+	# 	plt.xlabel('Time')
+	# 	plt.ylabel(fields[k])
+	# 	plt.show()
 	return
 
 def is_number(s):
@@ -78,14 +92,39 @@ def is_number(s):
     except ValueError:
         return False
 
-matrix_data = read_text_file_data()
-plot_sensor_data(matrix_data)
-
-# def read_stream_data(streamname):
-# 	pass
+def read_stream_data():
+	ser = serial.Serial()
+	ser.port = '/dev/serial/by-id/usb-Texas_Instruments_XDS110__02.02.05.01__with_CMSIS-DAP_L3000408-if00'
+	ser.baudrate = 115200
+	# ser.bytesize = 8
+	# ser.parity = 'N'
+	# ser.stopbits = 1
+	# ser.timeout = None
+	# ser.xonxoff = False
+	# ser.rtscts = False
+	# ser.dsrdtr = False
+	ser.open()
+	# print(ser)
+	data = []
+	# TODO: fix faulty input values of data, figure out when to start reading data, slow data rate down, get rid of accel data
+	while(1):
+		num = str(ser.readline().strip())
+		if(num != "b''"):
+			num2 = num[2:-1]
+			data.append(float(num2))
+		else:
+			gyro = data[0] + data[1] + data[2]
+			# print(gyro)
+			if(gyro >= 2):
+				print('In Use')
+			else:
+				print('Not in Use')
+			data = []
 
 # def process_data():
 # 	pass
 
-# process_data(read_text_file_to_numpy('example.txt'))
-# process_data(read_from_stream('stdin'))
+if __name__ == '__main__':
+	# matrix_data, _, _, _ = read_text_file_data(filename)
+	# plot_sensor_data(matrix_data)
+	read_stream_data()
--- senseable_gym/test/test_signal_processing.py
+++ senseable_gym/test/test_signal_processing.py
@@ -1,7 +1,7 @@
 import unittest
 
 # Package Imports
-from senseable_gym.sg_util.signal_processing import process_data
+from senseable_gym.sg_util.signal_processing import read_text_file_data
 
 class TestSignalProcessing(unittest.TestCase):
 	def setUp(self):
@@ -9,18 +9,37 @@ class TestSignalProcessing(unittest.TestCase):
 		pass
 
 	def test_read_text_file_data(self):
+		filename = './senseable_gym/test/data_txt_files/Weight_Machine'
+		_, no_newline, unwanted_newline, invalid_number = read_text_file_data(filename)
+		print('Filename: ' + filename)
+		print('NO NEWLINE WHERE THERE SHOULD BE ONE:')
+		for line in no_newline:
+			print('- Line ' + str(line))
+		print('NEWLINE WHERE THERE SHOULD NOT BE ONE:')
+		for line in unwanted_newline:
+			print('- Line ' + str(line))
+		print('LINE MUST CONTAIN A VALID NUMBER:')
+		for line in invalid_number:
+			print('- Line ' + str(line))
+
+	def test_plot_sensor_data(self):
 		pass
 
-	def test_plot_sensor_data():
+	def test_is_number(self):
 		pass
 
-	def test_is_number():
+	def test_is_processing_active_machine(self):
 		pass
+		# filename = './test/active_machine'
+
+		# process here
+		# result = True
+
+		# self.assertEqual(result, True)
 
 	def tearDown(self):
 		# Do this after I run every test
 		pass
-
 
 if __name__ == '__main__':
 	unittest.main()
