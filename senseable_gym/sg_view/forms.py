c60de470bdf29417c10673239a68c536ebf32e68
TJ DeVries <devries.timothyj@gmail.com>
2016-03-05 22:42:35 -0500

    Add a Specific Machine Form
    Also cleaned up for PEP8

--- senseable_gym/sg_view/forms.py
+++ senseable_gym/sg_view/forms.py
@@ -7,24 +7,32 @@ from wtforms.fields.html5 import DateField
 from wtforms_components import TimeField
 
 
-
 class MyForm(Form):
     name = StringField('name', validators=[DataRequired()])
-    
+
+
 class SignupForm(Form):
     user_name = TextField('User Name', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     repeat_pass = PasswordField('Repeat Password', validators=[DataRequired()])
     first_name = TextField('First Name', validators=[DataRequired()])
     last_name = TextField('Last Name', validators=[DataRequired()])
-    
+
+
 class LoginForm(Form):
     user = TextField('User', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me', default=False)
-    
+
+
 class ReserveForm(Form):
     machine = SelectField('Machine', coerce=int)
     date = DateField('Date', format='%Y-%m-%d', default=datetime.today())
-    start_time = TimeField('Start time', default = datetime.now())
+    start_time = TimeField('Start time', default=datetime.now())
+    length = IntegerField('Duration in minutes', default=30)
+
+
+class ReserveMachineForm(Form):
+    date = DateField('Date', format='%Y-%m-%d', default=datetime.today())
+    start_time = TimeField('Start time', default=datetime.now())
     length = IntegerField('Duration in minutes', default=30)
