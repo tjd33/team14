a01f2ddb874c8087f0a5558249b2c0f07885e458
TJ DeVries <devries.timothyj@gmail.com>
2016-04-03 18:47:53 -0400

    Add a machine updater configuration file
    This file will contain configuration defaults. At some point, we might
    need to add some command line options for easier runtime configuration,
    but this should really make things quite a lot easier to get ready for
    the presentation.

--- senseable_gym/sg_view/__init__.py
+++ senseable_gym/sg_view/__init__.py
@@ -18,18 +18,32 @@ database = main(level='INFO', dbname='webTest')
 # TODO: Include database configuration here
 login_manager = LoginManager()
 login_manager.init_app(app)
+
+
 @login_manager.user_loader
 def load_user(user_id):
     try:
         user = database.get_user_from_user_name(user_id)
     except:
         user = None
-    return user 
-    
+    return user
+
 # TODO: Include openID configuration here
 # TODO: Include logging configuration here
 
 # TODO: Configure jinja_env here
 # TODO: Import view-wide modules
+
+# Configure and start the machine status checker
+import configparser
+parsed_config = configparser.ConfigParser()
+parsed_config.read('./senseable_gym/sg_run/machine_updater.ini')
+
+if parsed_config['DEFAULT']['method'].lower() == 'serial':
+    # Call the serial updater
+    pass
+else:
+    # Other updater scenarios
+    pass
 
 from senseable_gym.sg_view import views
