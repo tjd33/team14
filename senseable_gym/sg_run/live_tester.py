c21b91de0f86fa41e4e7d8bcda41fbb306428c62
TJ DeVries <devries.timothyj@gmail.com>
2016-04-06 15:49:06 -0400

    Fix incorrect import for live testers

--- senseable_gym/sg_run/live_tester.py
+++ senseable_gym/sg_run/live_tester.py
@@ -1,15 +1,12 @@
-
-
-''' Standard Imports '''
+# Standard Imports
 import configparser
 
-''' Senseable Gym Imports'''
+# Senseable Gym Imports
 from senseable_gym.sg_util.signal_processing import StreamProcesser
 
 config = configparser.ConfigParser()
 config.read('./senseable_gym/sg_run/machine_updater.ini')
 
-s = StreamProcess(config['SERIAL']['port'], config['SERIAL']['baudrate'])
+s = StreamProcesser(config['SERIAL']['port'], config['SERIAL']['baudrate'])
 
 print(s.read_incremental())
-
