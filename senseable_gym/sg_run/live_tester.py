22ee55e47a851f305bf757b6287e4dcdc133eea0
TJ DeVries <devries.timothyj@gmail.com>
2016-04-06 19:14:13 +0000

    Add the start of a live testing module

---
+++ senseable_gym/sg_run/live_tester.py
@@ -0,0 +1,15 @@
+
+
+''' Standard Imports '''
+import configparser
+
+''' Senseable Gym Imports'''
+from senseable_gym.sg_util.signal_processing import StreamProcesser
+
+config = configparser.ConfigParser()
+config.read('./senseable_gym/sg_run/machine_updater.ini')
+
+s = StreamProcess(config['SERIAL']['port'], config['SERIAL']['baudrate'])
+
+print(s.read_incremental())
+
