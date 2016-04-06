53aafae0ec67a6b220e7d9692505ea7768080311
TJ DeVries <devries.timothyj@gmail.com>
2016-04-06 15:52:08 -0400

    Catch error if USB not connected

--- senseable_gym/sg_run/live_tester.py
+++ senseable_gym/sg_run/live_tester.py
@@ -2,11 +2,12 @@
 import configparser
 
 # Senseable Gym Imports
-from senseable_gym.sg_util.signal_processing import StreamProcesser
-
+from senseable_gym.sg_util.signal_processing import StreamProcessor
 config = configparser.ConfigParser()
 config.read('./senseable_gym/sg_run/machine_updater.ini')
 
-s = StreamProcesser(config['SERIAL']['port'], config['SERIAL']['baudrate'])
-
-print(s.read_incremental())
+try:
+    s = StreamProcessor(config['SERIAL']['port'], config['SERIAL']['baudrate'])
+    print(s.read_incremental())
+except Exception:
+    print('Most likely port is not connected')
