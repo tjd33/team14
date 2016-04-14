45027ddd314040981e0f13009489fc24817beac3
Daniel Dehoog <danielgdehoog@gmail.com>
2016-04-13 22:46:33 -0400

    added protection for the ajax updates

--- senseable_gym/sg_run/machine_updater.py
+++ senseable_gym/sg_run/machine_updater.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import requests
+from requests import Session
 
 
-def send_update(machine_id: int, machine_status: int, debug=False):
+def login(s, username, password, debug = False):
+    r = s.post("http://localhost:5000/login", data={'action': 'login', 'user': username, 'password': password })
+    if debug:
+        print(r.status_code, r.reason)
+        print(r.text[:300] + '...')
+
+def send_update(s, machine_id: int, machine_status: int, debug=False):
     # TODO: Use a real post request
     # TODO: Parameterize the url?
-    r = requests.get('http://127.0.0.1:5000/_update_status/{0}/{1}'.format(
+    r = s.get('http://127.0.0.1:5000/_update_status?id={0}&status={1}&pass=ajax_update'.format(
         machine_id,
         machine_status)
     )
@@ -17,5 +23,7 @@ def send_update(machine_id: int, machine_status: int, debug=False):
 
 
 if __name__ == "__main__":
-    send_update(1, 2, True)
-    send_update(2, 3, True)
+    with Session() as s:
+        # login(s, 'machine_update_user', 'ajax_update')
+        send_update(s, 1, 3, True)
+        send_update(s, 2, 2, True)
--- senseable_gym/sg_view/config.py
+++ senseable_gym/sg_view/config.py
@@ -1,5 +1,5 @@
 # Configuration settings for the current server
 DEBUG = False
 
-WTF_CSRF_ENABLED = True
+WTF_CSRF_ENABLED = False
 SECRET_KEY = 'you-will-never-guess'
