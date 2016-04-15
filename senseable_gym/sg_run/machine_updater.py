e36977449771551b218668fe472cd3f6a2b36f2c
Daniel Dehoog <danielgdehoog@gmail.com>
2016-04-15 15:11:39 -0400

    added config reading to machine_updater and create CEAC_tester

--- senseable_gym/sg_run/machine_updater.py
+++ senseable_gym/sg_run/machine_updater.py
@@ -2,6 +2,13 @@
 # -*- coding: utf-8 -*-
 
 from requests import Session
+import configparser
+
+config = configparser.ConfigParser()
+config.read('senseable_gym/sg_run/machine_updater.ini')
+ip = config['SERVER']['ip']
+port = config['SERVER']['port']
+password = config['SERVER']['pass']
 
 
 def login(s, username, password, debug = False):
@@ -13,9 +20,12 @@ def login(s, username, password, debug = False):
 def send_update(s, machine_id: int, machine_status: int, debug=False):
     # TODO: Use a real post request
     # TODO: Parameterize the url?
-    r = s.get('http://127.0.0.1:5000/_update_status?id={0}&status={1}&pass=ajax_update'.format(
+    r = s.get('http://{0}:{1}/_update_status?id={2}&status={3}&pass={4}'.format(
+        ip,
+        port,
         machine_id,
-        machine_status)
+        machine_status,
+        password)
     )
 
     if debug:
@@ -25,5 +35,7 @@ def send_update(s, machine_id: int, machine_status: int, debug=False):
 if __name__ == "__main__":
     with Session() as s:
         # login(s, 'machine_update_user', 'ajax_update')
-        send_update(s, 1, 3, True)
-        send_update(s, 2, 2, True)
+        send_update(s, 1, 2, True)
+        send_update(s, 2, 3, True)
+        
+        
